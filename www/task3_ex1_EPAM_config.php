75265b3898134d8f823089c2b75ece8bd66427d2
hamster1987 <january16@tut.by>
2014-08-07 11:04:03 +0300

    приступим..

--- www/task3_ex1_EPAM_config.php
+++ www/task3_ex1_EPAM_config.php
@@ -1,1 +1,7 @@
+<pre>
 <?php
+$users = array ('login'=> array ('password'=>'', 'allowed'=> array('zip'=> '1000', 'jpg' => '2000'),'home'=>'login', 'limit'=> '10000'),
+				'login2'=> array ('password'=>'', 'allowed'=> array('txt'=> '1000', 'jpg' => '2000'),'home'=>'login', 'limit'=> '10000'));
+print_r($users);
+?>
+</pre>
