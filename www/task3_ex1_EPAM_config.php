06c0345490a2e800a3a6bbd621fa128d977359a9
hamster1987 <january16@tut.by>
2014-08-12 19:31:13 +0300

    теперь закончил

--- www/task3_ex1_EPAM_config.php
+++ www/task3_ex1_EPAM_config.php
@@ -1,5 +1,5 @@
 <?php
-$users = array ('login'=> array ('password'=>'l1', 'allowed'=> array('zip'=> '1', 'jpg' => '2'),'home'=>'login', 'limit'=> '1'),
+$users = array ('login'=> array ('password'=>'l1', 'allowed'=> array('zip'=> '1', 'jpg' => '2'),'home'=>'login', 'limit'=> '4'),
 				'login2'=> array ('password'=>'l2', 'allowed'=> array('txt'=> '1', 'jpg' => '2'),'home'=>'login2', 'limit'=> '10'));
 //print_r($users);
 ?>
