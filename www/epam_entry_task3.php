3d106fe1bd7445bad2eb5f0ad7092b52d1ee8a14
hamster1987 <january16@tut.by>
2014-10-03 12:59:19 +0300

    протестировал

--- www/epam_entry_task3.php
+++ www/epam_entry_task3.php
@@ -1,43 +1,44 @@
 <?php
 
 //для тестов
-checkInput('12.4ю.qw.нгнкоadfrwgwgf......................sgffgdfgrgghtteQW...E!@..@$.%%@^яя.яяя.)((*&^%');
-checkInput('124qweQWE...');
-checkInput('12');
-checkInput('');
-checkInput('fg.wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-	wwwwwwwwwwwwwwwwwwwwwwwwwwwwttttwwwwwwwwwwwwwwwwwwwwwww
-	wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-	wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww200ffffffffffggggggggggg
-	gggggggggggggggggggggggggggggggggggggggg
-	hhhhhhhhhhhhhhhhjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-	jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-	kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk');
-checkInput('fgwwwwwjjjj
-	jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-	kkkkkkkkkkkkkkkkkkkkkkkkkk.kkkkkkkkkkkkkkkj5gdhdntr5hbe5574jjl;i[///.....ytttyyyyyyyyyyyyyyyyyykkkkkkkkkkkkk');
-checkInput('fgwwwwwjjjj
-	jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-	kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkj5gdhdntr5hbe5574jjl;i[///ytttyyyyyyyyyyyyyyyyyykkkkkkkkkkkkk
-	gggggggggggggggggggggggggggggggggggggggggggggggggg
-	ggggggggggggggggggggggggggggggggggggggggggggggggggggg
-	ggggggggggggggggggggggggggggggggggttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttend');
+echo checkInput('echo "bang")//;');
+echo checkInput('www.exe');
+echo checkInput('w......ww..ex.e');
+echo checkInput('!@#$%^&*()_+.)(**&^%$#@@AEWQghj');
+echo checkInput('Русские буквы');
+echo checkInput('BIG LETTERS');
+echo checkInput('12');
+echo checkInput('');
+echo checkInput('w
+	ww/r//n
 
+	www
+
+
+	wwww
+		wwwww
+	wwwww');
+echo checkInput('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww50symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww100symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww150symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww200symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww250symbols.wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww50symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwttttwwwwwwwwwwwwwwwwwwwwwww
+	wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+	wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww200symbolsffffffffffffffffffffffff');
+echo checkInput('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww50symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww100symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww150symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww200symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww250symbols');
 //искомая функция
 function checkInput($str)
 {
 	$str = preg_replace('/[^a-z_0-9.]/', "", $str);// удаляем все недопустимые символы
 	$str = preg_replace('/((?<=\.)[^.]*)\./', '$1', $str);// если перед и после любого количества любых символов И точки есть точка, убрать ее. 
 	$str = preg_replace('/((?<=\.).{50}).*/', '$1', $str);// не удалять 50 символов после точки, остальные после этих 50 - Weg!
-	if ((strlen($str) > 200)&&(strpos($str,'.')))// если есть точка, и строка длинная, то минимально порежем конец имени файла
+	if ((strlen($str) > 200)&&(strpos($str,'.')))// если есть точка, и строка длинная, то минимально порежем конец имени файла, а не расширение
 	{
 		$nameExt=explode('.', $str);
 		$name = $nameExt[0];
 		$extension = $nameExt[1];
 		$name = substr($name, 0, 200-strlen($extension)-1);
 		$str = $name.".".$extension;	
-	}else{
+	}
+	if (strlen($str) > 200)
+	{
 		$str = preg_replace('/(.{200}).*/', '$1', $str);//раз в названии нет точек, то отрезать все, что за пределами дозволенных 200 символов от начала
-	}	
-	echo $str.'<br>';
+	}
+	return $str.'<br>';
 }
