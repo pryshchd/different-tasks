b508864ab3398cabf88fc3401aa82d871fe9f35c
hamster1987 <january16@tut.by>
2014-10-05 13:51:42 +0300

    учел рекомендации

--- www/epam_entry_task3.php
+++ www/epam_entry_task3.php
@@ -1,6 +1,6 @@
 <?php
 
-//для тестов
+//for tests
 echo checkInput('echo "bang")//;') . '<br>';// FALSE
 echo checkInput('www.exe') . '<br>';// www.exe
 echo checkInput('w......ww..ex.e') . '<br>';// wwwex.e
@@ -19,17 +19,18 @@ wwwww
 echo checkInput('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww50symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww100symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww150symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww200symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww250symbols.wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww50symbolswwwwwwwwwwwwwwwwwwwwwwwwwwwwttttwwwwwwwwwwwwwwwwwwwwwww
 	wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
 	wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww200symbolsffffffffffffffffffffffff') . '<br>';//... 150symbol.www....
-//искомая функция - возвращает FALSE если имя файла или его расширение вообще не содержат допустимых символов.
+
+//the function itself follows below: returns FALSE if the filename or extension contain no permitted characters or no dots(.), i.e. cannot be rendered as a legal file name without adding something to it 
 function checkInput($str)
 {
-	$str = preg_replace('/[^a-z_0-9.]/', "", $str);// удаляем все недопустимые символы
-	$str = preg_replace('/\.(?=.*\.)/', '', $str);// если до любого количества любых символов И точки есть точка, убрать ее (оставляем последнюю точку). 
-	$str = preg_replace('/((?<=\.).{50}).*/', '$1', $str);// оставляем 50 символов после точки, остальные после этих 50 удаляем
-	$nameExt=explode('.', $str);//поделим строку на имя файла и его расширение
+	$str = preg_replace('/[^a-z_0-9.]/', "", $str);// remove all but permitted entities
+	$str = preg_replace('/\.(?=.*\.)/', "", $str);// if there is a dot preceding any numner of any entities and a dot, remove the first of the two dots (removes all but the last of the dots) 
+	$str = preg_replace('/((?<=\.).{50}).*/', "$1", $str);// leave only 50 characters after the dot, remove the rest of the characters following after the dot
+	$nameExt=explode('.', $str);// divide the string into two parts: file name and file extension
 	$name = $nameExt[0];
 	$extension = $nameExt[1];
-	if ($name == '' || $extension == '' || strpos($str, '.') == NULL) return FALSE;//если имя файла или его расширение не содержат символов, вернем FALSE
-	if (strlen($str) > 200)// если  строка длинная, то минимально порежем конец имени файла, а не расширение
+	if ($name == '' || $extension == '' || strpos($str, '.') == NULL) return FALSE;// return FALSE in eithe case: file name is empty, file extension is empty, no dots contained in the string (cannot separate file name from its extension) 
+	if (strlen($str) > 200)//if the string is too long, remove as little as possible from its name (the extension is already no longer than 50 characters) 
 	{
 		$name = substr($name, 0, 200-strlen($extension)-1);
 	}
